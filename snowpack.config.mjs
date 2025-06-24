18ce398b0d73f58fe68cd709c086d26ab7477c13
Daniel Gasienica <daniel@gasienica.ch>
2025-06-24 10:03:09 +0200

    Restore original Snowpack build location

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -6,7 +6,7 @@ export default {
     src: "/__DELETE_ME__/haskell",
   },
   buildOptions: {
-    out: "frontend/build",
+    out: "./frontend/build",
   },
   devOptions: {
     output: "stream", // don't clear terminal
